f1690fb6c0061d5969c974ce72b6f7442c30f55d
Carlos Henrique <carlinhosbackend@gmail.com>
2025-10-16 23:39:30 -0300

    feat: Scaffold e appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // Meu primeiro aplicativo ein kct ksksks
-  runApp(MaterialApp(home: Center(child: Text('Olá mundo'))));
+  // Meu codigo em flutter
+  runApp(
+      MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Eu sou Celso'),
+        backgroundColor: Colors.red[600],),),
+  ));
 }
